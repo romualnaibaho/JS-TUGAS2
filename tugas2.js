9e65fbdaab2ffa2a08be8214ffe730ed72f62959
Romual Naibaho <romualnaibaho26@gmail.com>
2021-06-30 22:34:52 +0700

    js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 12;
+var string = "Dua Belas";
+var boolean = true;
+var kosong = null;
+var array = [2,3,4,5,6];
+
+console.log("Number ->", number);
+console.log("String ->", string);
+console.log("Boolean ->", boolean);
+console.log("Kosong ->", kosong);
+console.log("Array ->", array);
